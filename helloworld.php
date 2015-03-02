268af4e7895963f0395d1e5e3ea0473830c1d01c
Cristian Sanchez <sancas19@gmail.com>
2015-03-01 23:34:04 -0600

    Agregando archvivos view y separando logica y presentacion

--- helloworld.php
+++ helloworld.php
@@ -1,11 +1,5 @@
-<?php $language = "PHP"; ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<h1>Mejorando.la</h1>
-<?php echo "Aprendiendo <strong>$language</strong>"; ?>
-</body>
-</html>
+<?php
+
+	$language = "PHP";
+
+	require "view.php";
---
+++ view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Mejorando.la</h1>
+Aprendiendo <strong><?= $language ?></strong>
+</body>
+</html>
