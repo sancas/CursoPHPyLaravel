4e33418ccab3c6691c20746e1d80370844f8577f
Cristian Sanchez <sancas19@gmail.com>
2015-03-01 23:20:11 -0600

    Empezando con el segundo video

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello World";
-?>
+<?php $language = "PHP"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Mejorando.la</h1>
+<?php echo "Aprendiendo <strong>$language</strong>"; ?>
+</body>
+</html>
